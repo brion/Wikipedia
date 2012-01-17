d50cfb2884498de79b5a58e82feb494e26fda17e
Brion Vibber <brion@pobox.com>
2012-01-17 13:49:39 -0800

    don't recreate map if already made it

--- assets/www/js/geo.js
+++ assets/www/js/geo.js
@@ -1,21 +1,25 @@
+var geomap = false;
+
 function getCurrentPosition() {
 	hideOverlayDivs();
 	hideContentIfNeeded();
 	$("#nearby-overlay").localize().show();
 	setActiveState();
 
-	var map = new L.Map('map');
-	var tiles = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; 2011 OpenStreetMap contributors'
-	});
+	if (!geomap) {
+		geomap = new L.Map('map');
+		var tiles = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; 2011 OpenStreetMap contributors'
+		});
+		geomap.addLayer(tiles);
 
-	// @fixme load last-seen coordinates
-	map.setView(new L.LatLng(0, 0), 13);
-	map.addLayer(tiles);
+		// @fixme load last-seen coordinates
+		geomap.setView(new L.LatLng(0, 0), 13);
+	}
 
 	navigator.geolocation.getCurrentPosition(function(pos) {
-		map.setView(new L.LatLng(pos.coords.latitude, pos.coords.longitude), 13);
+		geomap.setView(new L.LatLng(pos.coords.latitude, pos.coords.longitude), 13);
 	});
 }
 
