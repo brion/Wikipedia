3c790ff8b084336d6c323d58358022254753a173
Brion Vibber <brion@pobox.com>
2012-01-17 13:28:43 -0800

    map: first stab at attempting to pull location... loading tiles from OSM master

--- assets/www/js/geo.js
+++ assets/www/js/geo.js
@@ -5,6 +5,18 @@ function getCurrentPosition() {
 	setActiveState();
 
 	var map = new L.Map('map');
-	
+    var tiles = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; 2011 OpenStreetMap contributors'
+    });
+
+    // @fixme load last-seen coordinates
+    map.setView(new L.LatLng(0, 0), 13);
+    map.addLayer(tiles);
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+		alert(pos.coords.latitude + pos.coords.longitude);
+		map.setView(new L.LatLng(pos.coords.latitude, pos.coords.longitude), 13);
+    });
 }
 
