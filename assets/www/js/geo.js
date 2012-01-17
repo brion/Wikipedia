43b1a67f916ebb49effb24f70b67dcc38af37046
Brion Vibber <brion@pobox.com>
2012-01-17 13:11:49 -0800

    start switching around to default to an html map

--- assets/www/js/geo.js
+++ assets/www/js/geo.js
@@ -1,28 +1,10 @@
 function getCurrentPosition() {
-	PhoneGap.exec(geoNameSuccess, geoNameFailure, "NearMePlugin", "startNearMeActivity", [preferencesDB.get('language')]);
+	hideOverlayDivs();
+	hideContentIfNeeded();
+	$("#nearby-overlay").localize().show();
+	setActiveState();
+
+	var map = new L.Map('map');
+	
 }
 
-function geoNameSuccess(wikipediaUrl) {
-	if(wikipediaUrl) {
-		$('#search').addClass('inProgress');
-		$.ajax({url: "https://en.m.wikipedia.org",
-			success: function(data) {
-				if(data) {
-					app.navigateToPage('https://'+wikipediaUrl)
-				} else {
-					noConnectionMsg();
-					navigator.app.exitApp();
-				}
-			},
-			error: function(xhr) {
-				noConnectionMsg();
-			},
-			timeout: 3000
-		});
-	}
-}
-
-function geoNameFailure(error) {
-	console.log(error);
-	alert('Google Maps service is not available on this device.');
-}
