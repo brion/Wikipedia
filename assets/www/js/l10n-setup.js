672921e9d7571699ccdb58b66571ac93d489cf56
Brion Vibber <brion@pobox.com>
2012-04-24 00:49:38 +0000

    fix for loading messages on winphone7
    async in the .ajax call killed it; also using a full URL seems to help

--- assets/www/js/l10n-setup.js
+++ assets/www/js/l10n-setup.js
@@ -37,11 +37,11 @@ window.l10n = function() {
 	function loadMessages(lang, callback) {
 		//console.log('loading messages for ' + lang);
 		lang = normalizeLanguageCode(lang);
-		var url = 'messages/messages-' + lang + '.properties';
+		var url = ROOT_URL + 'messages/messages-' + lang + '.properties';
 		console.log('Loading messages: ' + url);
 		$.ajax({
 			url: url,
-			async: false,
+			//async: false, // fails on WinPhone7.1
 			dataType: 'text',
 			success: function(data) {
 				console.log('success loading ' + url);
